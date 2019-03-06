77dae3494ae5cc1afd41866d42ba86a65d05ac5d
vishang soni <vishangsoni@gmail.com>
2019-03-06 18:50:29 -0500

    Playing with tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// this will ensure they're removed after each test.
+import 'react-testing-library/cleanup-after-each';
+// this adds jest-dom's custom assertions
+import 'jest-dom/extend-expect';
 
 configure({ adapter: new Adapter() });
