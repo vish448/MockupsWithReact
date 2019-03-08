49e49bb255710ac1e076048c8acf1b3dd5850842
vishang soni <vishangsoni@gmail.com>
2019-03-08 17:08:28 -0500

    adding input and button components

--- src/mockups/google/components/container.js
+++ src/mockups/google/components/container.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import googleLogo from '../../../images/googleLogo.png'
+import SearchBar from '../../../components/searchBar'
+import Button from '../../../components/button'
+
+
 export default class Container extends React.Component {
   render (){
     return (
-      <div className="container">Container</div>
+      <div className="container">
+        <div className="logo">
+          <img src={googleLogo} alt="googleLogo" width="400" />
+        </div>
+        <div className="searchBar">
+          <SearchBar />
+        </div>
+        <div className="buttons">
+          <div className="buttonContainer">
+            Button
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/mockups/google/google.js
+++ src/mockups/google/google.js
@@ -9,23 +9,6 @@ export default class Google extends Component {
   render() {
     return (
       <div id="googleMockup">
-        <h1>google</h1>
-
-        // TODO: Dump components
-        /*
-          Local header
-            Navigation Left
-            Navigation Right
-          Container
-            Google logo
-            Search Bar
-            Buttons
-          Footer
-            Country Footer
-            FooterNavigation Left
-            FooterNavigation Right
-
-        */
         <Header />
         <Container />
         <Footer />
