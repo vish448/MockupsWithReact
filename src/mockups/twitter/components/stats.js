2419597f84339fc216e1ab1f48751a479bdedb76
vishang soni <vishangsoni@gmail.com>
2019-03-14 13:13:55 -0400

    adding stat navigation link and button

--- src/mockups/twitter/components/stats.js
+++ src/mockups/twitter/components/stats.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+
 export default class Stats extends React.Component {
   render (){
     return (
       <div id="stats">
-        
+          <div id="span20"></div>
+          <div id="statNav">
+            <ul>
+              <li>
+                <Link to="/tweet">Tweet<br/>20</Link>
+              </li>
+              <li>
+                <Link to="/follwing">Following<br/>22</Link>
+              </li>
+              <li>
+                <Link to="/followers">Followers<br/>10</Link>
+              </li>
+              <li>
+                <Link to="/likes">Likes<br/>74</Link>
+              </li>
+              <li>
+                <Link to="/lists">Lists<br/>4</Link>
+              </li>
+              <li>
+                <Link to="/moments">Moments<br/>20</Link>
+              </li>
+            </ul>
+            <button className="clear editProfile">Edit Profile</button>
+
+          </div>
       </div>
     )
   }
