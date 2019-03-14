34b7a29158fa737d9622c35eb051c0ce22a21b58
vishang soni <vishangsoni@gmail.com>
2019-03-13 23:09:58 -0400

    adding links in the top navigation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import {
   Route,
 } from 'react-router-dom'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faHome,faBolt,faBell,faEnvelope,faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
+
 import Home from './mockups/home'
 import Google from './mockups/google/google'
 import Twitter from './mockups/twitter/twitter'
@@ -16,6 +22,8 @@ import Twitter from './mockups/twitter/twitter'
 
 class App extends Component {
   render() {
+    library.add(fab,faHome,faBolt,faBell,faEnvelope, faUserCircle)
+
     return (
       <BrowserRouter>
         <div className="App">
@@ -25,8 +33,7 @@ class App extends Component {
             <Route path="/twitter" component = {Twitter} />
         </div>
       </BrowserRouter>
-
-    );
+    )
   }
 }
 
