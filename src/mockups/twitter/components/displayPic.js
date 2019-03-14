d47d54ddbe15e10ebd1be6ae7263f9c947781578
vishang soni <vishangsoni@gmail.com>
2019-03-14 12:13:35 -0400

    adding display pic image

--- src/mockups/twitter/components/displayPic.js
+++ src/mockups/twitter/components/displayPic.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import vishDP from '../../../images/vishangsoni.jpg'
+
 export default class DisplayPic extends React.Component {
   render (){
     return (
       <div id="displayPic">
+        <img src= {vishDP} />
       </div>
     )
   }
